cc2255da622619601d8e0907e7ce51bf467684ea
PatrykCzudak <patrykczudak@gmail.com>
2024-12-14 15:52:12 +0100

    zabawy db

--- db_connection.py
+++ db_connection.py
@@ -1,8 +1,13 @@
 import sqlite3
 
-db = sqlite3.connect('movies.db')
-cursor = db.cursor()
-cursor.execute('SELECT * FROM movies')
-for row in cursor:
-    print('{0} {1} {2}'.format(row[1], row[2], row[3]))
-db.close()
+def get_data_from_db(title:str) -> None:
+    title = f"%{title}%"
+    db = sqlite3.connect('movies.db')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM movies WHERE title LIKE ?", (title,))
+    for row in cursor:
+        print('{0} {1} {2}'.format(row[1], row[2], row[3]))
+    db.close()
+
+
+print(get_data_from_db("a"))
